2bbf06b58d01100d4f48c7d2865903c8c49705fb
Joshua <softboyjosh27@gmail.com>
2026-02-12 11:12:47 +0100

    Add additional movie entries to the database

--- src/data/db.js
+++ src/data/db.js
@@ -5,7 +5,7 @@ export const users = [
         "lastName":'Doe',
         "email": "johndoe@gmail.com",
         "password": "password"
-},
+}, 
 {
     "id": 2,
     "firstName": "Joshua",
@@ -26,7 +26,25 @@ export const movies = [
     title: "SpiderMan",
     year: 2026,
     createdAt: new Date("2024-01-15T10:00:00Z")
-  }
+  },
+   {
+    id: 3,
+    title: "Anikulapo",
+    year: 2025,
+    createdAt: new Date("2024-01-15T10:00:00Z")
+  },
+   {
+    id: 4,
+    title: "365 days",
+    year: 2025,
+    createdAt: new Date("2024-01-15T10:00:00Z")
+  },
+   {
+    id: 5,
+    title: "BEN 10",
+    year: 2025,
+    createdAt: new Date("2024-01-15T10:00:00Z")
+  },
 ];
 
 export const watchlists = [
